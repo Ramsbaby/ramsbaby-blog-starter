56c6d00252e574ac64319bb4ae5db37960326f70
Ramsbaby <ms6698@naver.com>
2020-12-21 00:25:12 +0900

    배경이펙트수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,13 +88,6 @@ export default ({ data, location }) => {
           aria-hidden=""
           params={{
             particles: {
-              number: {
-                value: 30,
-                density: {
-                  enable: true,
-                  value_area: 800,
-                },
-              },
               color: {
                 value: '#FFE08C',
               },
@@ -112,88 +105,42 @@ export default ({ data, location }) => {
                   height: 100,
                 },
               },
-              opacity: {
-                value: 0.5,
-                random: true,
-                anim: {
-                  enable: true,
-                  speed: 0.5,
-                  opacity_min: 0.1,
-                  sync: false,
+              number: {
+                value: 200,
+                density: {
+                  enable: false,
                 },
               },
               size: {
-                value: 1,
+                value: 10,
                 random: true,
-                anim: {
-                  enable: true,
-                  speed: 0,
-                  size_min: 0,
-                  sync: false,
-                },
-              },
-              line_linked: {
-                enable: true,
-                distance: 300,
-                color: '#ff0000',
-                opacity: 0.2,
-                width: 1,
               },
               move: {
                 enable: true,
-                speed: 0.5,
-                direction: 'none',
+                speed: 1,
+                direction: 'bottom',
                 random: true,
                 straight: false,
-                out_mode: 'bounce',
+                out_mode: 'out',
                 bounce: false,
-                attract: {
-                  enable: true,
-                  rotateX: 600,
-                  rotateY: 1200,
-                },
+              },
+              line_linked: {
+                enable: false,
               },
             },
             interactivity: {
-              detect_on: 'canvas',
               events: {
-                onhover: {
-                  enable: true,
-                  mode: 'repulse',
-                },
                 onclick: {
                   enable: true,
-                  mode: 'push',
+                  mode: 'remove',
                 },
-                resize: true,
               },
               modes: {
-                grab: {
-                  distance: 400,
-                  line_linked: {
-                    opacity: 1,
-                  },
-                },
-                bubble: {
-                  distance: 400,
-                  size: 40,
-                  duration: 2,
-                  opacity: 8,
-                  speed: 3,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-                push: {
-                  particles_nb: 4,
-                },
                 remove: {
-                  particles_nb: 2,
+                  particles_nb: 10,
                 },
               },
             },
-            retina_detect: false,
           }}
         ></Particles>
         <div>
