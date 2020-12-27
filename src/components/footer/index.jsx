c7986cd6460ec3b01c0fbd3ad1153b89cc2bcf16
Ramsbaby <ms6698@naver.com>
2020-12-28 00:45:53 +0900

    edit

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const Footer = () => (
   <footer className="footer">
     ©<a href="https://github.com/Ramsbaby">Ramsbaby</a>, Built with{' '}
-    <a href="https://github.com/Ramsbaby/ramsbaby-blog-starter">
+    <a href="https://github.com/LeeAndJang/L-J-gatsby-blog-starter">
       ramsbaby-blog-starter
     </a>
   </footer>
