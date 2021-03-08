4a689cabbd201bfe73bbf6ad84d9b8413cac28e9
RAMSBABY <ms6698@naver.com>
2021-03-09 02:50:12 +0900

    ga api test

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,10 +1,47 @@
 import React, { useState, useEffect } from 'react'
 import GoogleLogin from 'react-google-login'
+import gapi from '../utils/gapi.js'
 
 class Test extends React.Component {
   render() {
     const responseGoogle = response => {
       console.log(response)
+    }
+
+    var VIEW_ID = '229844781'
+
+    // Query the API and print the results to the page.
+    function queryReports() {
+      gapi.client
+        .request({
+          path: '/v4/reports:batchGet',
+          root: 'https://analyticsreporting.googleapis.com/',
+          method: 'POST',
+          body: {
+            reportRequests: [
+              {
+                viewId: VIEW_ID,
+                dateRanges: [
+                  {
+                    startDate: '2020-09-01',
+                    endDate: 'today',
+                  },
+                ],
+                metrics: [
+                  {
+                    expression: 'ga:users',
+                  },
+                ],
+              },
+            ],
+          },
+        })
+        .then(this.displayResults, location.reload())
+    }
+
+    function displayResults(response) {
+      var formattedJson = JSON.stringify(response.result, null, 2)
+      document.getElementById('query-output').value = formattedJson
     }
 
     return (
@@ -20,17 +57,20 @@ class Test extends React.Component {
               name="google-signin-scope"
               content="https://www.googleapis.com/auth/analytics.readonly"
             />
+
+            {/* <!-- Load the JavaScript API client and Sign-in library. --> */}
+            {/* <script src="https://apis.google.com/js/client:platform.js"></script> */}
           </head>
           <body>
             <h1>Hello Analytics Reporting API V4</h1>
 
             {/* <!-- The Sign-in button. This will run `queryReports()` on success. --> */}
-            {/* <p class="g-signin2" data-onsuccess="queryReports"></p> */}
+            <p class="g-signin2" data-onsuccess="queryReports"></p>
 
             <GoogleLogin
               clientId="1075573877493-1f6ev81d3v2aq4caf22qv3e9h9s78d9i.apps.googleusercontent.com"
               buttonText="Login"
-              onSuccess={this.queryReports}
+              onSuccess={queryReports}
               onFailure={responseGoogle}
               cookiePolicy={'single_host_origin'}
             />
@@ -82,9 +122,6 @@ class Test extends React.Component {
       `,
               }}
             />
-
-            {/* <!-- Load the JavaScript API client and Sign-in library. --> */}
-            <script src="https://apis.google.com/js/client:platform.js"></script>
           </body>
         </html>
       </div>
