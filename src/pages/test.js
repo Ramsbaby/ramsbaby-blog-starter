46b436bf33b4b561856e0ba9de50c2f7a79c8c30
RAMSBABY <ms6698@naver.com>
2021-03-09 00:41:43 +0900

    ga api test

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,8 +4,6 @@ class Test extends React.Component {
   render() {
     // Replace with your view ID. */
 
-    var VIEW_ID = '229844781'
-
     function displayResults(response) {
       var formattedJson = JSON.stringify(response.result, null, 2)
       document.getElementById('query-output').value = formattedJson
@@ -36,35 +34,36 @@ class Test extends React.Component {
           <script
             dangerouslySetInnerHTML={{
               __html: `
+              var VIEW_ID = '229844781'
 
-  // Query the API and print the results to the page.
-  function queryReports() {
-    gapi.client
-      .request({
-        path: "/v4/reports:batchGet",
-        root: "https://analyticsreporting.googleapis.com/",
-        method: "POST",
-        body: {
-          reportRequests: [
-            {
-              viewId: VIEW_ID,
-              dateRanges: [
-                {
-                  startDate: "7daysAgo",
-                  endDate: "today",
-                },
-              ],
-              metrics: [
-                {
-                  expression: "ga:sessions",
-                },
-              ],
-            },
-          ],
-        },
-      })
-      .then(displayResults, console.error.bind(console))
-  }
+              // Query the API and print the results to the page.
+              function queryReports() {
+                gapi.client
+                  .request({
+                    path: "/v4/reports:batchGet",
+                    root: "https://analyticsreporting.googleapis.com/",
+                    method: "POST",
+                    body: {
+                      reportRequests: [
+                        {
+                          viewId: VIEW_ID,
+                          dateRanges: [
+                            {
+                              startDate: "7daysAgo",
+                              endDate: "today",
+                            },
+                          ],
+                          metrics: [
+                            {
+                              expression: "ga:sessions",
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  })
+                  .then(displayResults, console.error.bind(console))
+              }
 
   
       `,
