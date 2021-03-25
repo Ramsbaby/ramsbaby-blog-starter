a2e1f5fc9fae064ad92bcdad4f7c046b1d3f9b31
RAMSBABY <ms6698@naver.com>
2021-03-25 21:03:18 +0900

    admin page test

--- src/utils/material-ui/components/Sidebar/Sidebar.js
+++ src/utils/material-ui/components/Sidebar/Sidebar.js
@@ -32,7 +32,7 @@ export default function Sidebar(props) {
 
   const { state, actions } = useContext(GlAuthContext)
 
-  console.log(gapi)
+  // console.log(gapi)
 
   // verifies if routeName is the one active (in browser input)
   function activeRoute(routeName) {
