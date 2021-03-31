46aee680eed85d68470124d766eff687db502617
RAMSBABY <ms6698@naver.com>
2021-03-31 21:52:22 +0900

    admin page test

--- src/pages/test.js
+++ src/pages/test.js
@@ -62,7 +62,11 @@ class Test extends React.Component {
             />
 
             {/* <!-- Load the JavaScript API client and Sign-in library. --> */}
-            <script src="https://apis.google.com/js/client:platform.js"></script>
+            <script
+              src="https://apis.google.com/js/client:platform.js?onload=init"
+              async
+              defer
+            ></script>
           </head>
           <body>
             <h1>Hello Analytics Reporting API V4</h1>
