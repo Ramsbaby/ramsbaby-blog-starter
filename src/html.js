c11c1ca66d1e1ea5f4e945c5e147c6ad600cd41c
RAMSBABY <ms6698@naver.com>
2021-04-01 01:48:55 +0900

    admin page test

--- src/html.js
+++ src/html.js
@@ -12,11 +12,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
-          <script
-            src="https://apis.google.com/js/platform.js?onload=renderButton"
-            async
-            defer
-          ></script>
+          <script src="https://apis.google.com/js/platform.js"></script>
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
